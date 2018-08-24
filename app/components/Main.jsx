2b0b9a9260d3aec14bf1a6c3b7f374bd45c4a3bb
afifasoft <abdul.pesit@gmail.com>
2018-08-24 12:50:42 +0530

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+var Nav = require('Nav');
+
+
+
+// var Main = React.createClass({
+//   render: function(){
+//     return(
+//       <div>
+//
+//         <Nav/>
+//         <h2>Main component</h2>
+//         {this.props.children}
+//
+//       </div>
+//     );
+//   }
+// });
+
+var Main = (props) => {
+  return(
+    <div>
+
+      <div>
+          <div>
+            <Nav/>
+            <p>Welcome</p>
+            {props.children}
+          </div>
+      </div>
+
+
+    </div>
+  );
+};
+
+module.exports = Main;
